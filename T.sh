26215d33f160a1039705135e328e7e90ebc0e636
Tim <96081549+Timnini@users.noreply.github.com>
2024-02-02 12:03:45 +0100

    Update T.sh

--- T.sh
+++ T.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-cat data.csv|cut -d ';' -f2,3 > townA2.txt
-cat data.csv|cut -d ';' -f2,4 > townB2.txt
+# Extraction des colonnes 2 et 3 du fichier data.csv et sauvegarde dans townA2.txt
+cat data.csv | cut -d ';' -f2,3 > townA2.txt
+
+# Extraction des colonnes 2 et 4 du fichier data.csv et sauvegarde dans townB2.txt
+cat data.csv | cut -d ';' -f2,4 > townB2.txt
+
+# Exécution du programme prog
 ./prog
 
+# Utilisation de gnuplot pour générer un graphique des 10 villes les plus traversées
 gnuplot << EOF
 set terminal png
-set output "histogramme_t.png"
+set output "graph_T.png"
 set title "Les 10 villes les plus traversées"
 set style data histogram
 set style fill solid
@@ -15,8 +21,15 @@ set xlabel "Noms des villes"
 set xtic rotate by -45
 set style histogram clustered
 set datafile separator ';'
+
+# Utilise deux colonnes pour représenter le nombre de routes depuis chaque ville
 plot "resultat_T.txt" using 2:xtic(1) title "Première ville", \
      '' using 3:xtic(1) title "routes totales"
-
 EOF
-xdg-open histogramme_t.png
+
+# Supprime les fichiers temporaires générés
+rm townA2.txt
+rm townB2.txt
+
+# Ouvre l'image générée avec le visualiseur d'image par défaut sur le système
+xdg-open graph_T.png
